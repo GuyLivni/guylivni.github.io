8991d7eeaba28e2187990abcc9ff4d5091b2f03e
GuyLivni <guylivni@gmail.com>
2019-03-26 13:12:54 +0100

    Updates

--- sw.js
+++ sw.js
@@ -35,14 +35,14 @@ self.__precacheManifest = [
     "url": "styles-23ca45fb2b0db2de8fe0.js"
   },
   {
-    "url": "app-7079eb764609da591489.js"
+    "url": "app-705919bf07ca9b2458ef.js"
   },
   {
     "url": "component---node-modules-gatsby-plugin-offline-app-shell-js-818aea6714ef07055821.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2d75cd9b060fbd32af1a74975dadef0b"
+    "revision": "8fea363783f04857b9ca11f68b83ca2c"
   },
   {
     "url": "component---src-pages-404-js-a63057719ddce55a4006.js"
