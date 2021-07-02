c8973538040aca576431db70c4a7a4e6dacee8cc
Guy Livni <guylivni@gmail.com>
2021-07-02 08:32:26 +0200

    refactor: rewrite Fetcher to ts

---
+++ src/components/Fetcher/Fetcher.tsx
@@ -0,0 +1,40 @@
+import React, { FC, ReactNode, ReactElement } from 'react';
+import { useFetch } from '../../hooks';
+import { general } from '../../constants';
+
+type Post = {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
+
+type Action = {
+  method: 'get' | 'post';
+  responseType: 'json';
+  url: string;
+};
+
+type FetcherProps = {
+  loader: ReactNode;
+  action: Action;
+  children(data: Array<{}>): ReactElement;
+};
+
+const DefaultLoader = () => <>{general.LOADING_TEXT}</>;
+
+const Fetcher: FC<FetcherProps> = ({
+  action,
+  loader = DefaultLoader,
+  children,
+}) => {
+  const { status, data, error } = useFetch<Post[]>(action.url);
+
+  if (status === 'fetching') return loader;
+  if (error) return <>{error}</>;
+  if (!data) return null;
+
+  return children(data);
+};
+
+export default Fetcher;
