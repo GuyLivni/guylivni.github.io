31349287f7813919e2c61574f02e6d4df00012b4
GuyLivni <guylivni@gmail.com>
2021-07-08 10:57:17 +0000

    Deploying to docs from @ GuyLivni/guylivni.github.io@34c2f8eb47ef8774b1218e21adf35566600ac201 🚀

--- sw.js
+++ sw.js
@@ -39,11 +39,11 @@ self.__precacheManifest = [
     "url": "1bfc9850-a82d8d7c90678518e793.js"
   },
   {
-    "url": "app-4271d1a7dc43f8426939.js"
+    "url": "app-db2869812f6a6ecccb5a.js"
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "b7b16eef1fa4e9c81716bfe7af818a1e"
+    "revision": "376980c8ec0d5ddf3ab8e52c2887c877"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-3d01b3844f0fa2d7f2a7.js"
@@ -145,7 +145,7 @@ const navigationRoute = new NavigationRoute(async ({ event }) => {
   // Check for resources + the app bundle
   // The latter may not exist if the SW is updating to a new version
   const resources = await idbKeyval.get(`resources:${pathname}`)
-  if (!resources || !(await caches.match(`/app-4271d1a7dc43f8426939.js`))) {
+  if (!resources || !(await caches.match(`/app-db2869812f6a6ecccb5a.js`))) {
     return await fetch(event.request)
   }
 
