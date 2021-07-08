c227d6c978cff588f461626a5a0239de88419201
Guy Livni <guylivni@gmail.com>
2021-07-08 12:15:05 +0200

    fix: remove mobile transition

--- src/components/Menu/MobileMenu/MobileMenu.tsx
+++ src/components/Menu/MobileMenu/MobileMenu.tsx
@@ -15,7 +15,7 @@ const MobileMenu = ({ location, routes }: MobileMenuProps) => {
     <TransitionPortal>
       <Container>
         {routes.map(({ Icon, path }, index) => (
-          <Link paintDrip key={index} to={path} pathname={location.pathname}>
+          <Link key={index} to={path} pathname={location.pathname}>
             <StyledIcon
               pathname={location.pathname}
               to={path}
